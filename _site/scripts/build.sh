20ee15af8a0c7a6bdbfba2a16e1e2f114b40e7a9
p13brif <p13brif@ionio.gr>
2021-04-03 04:37:33 +0300

    Update HTML

--- _site/scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-cd $TRAVIS_BUILD_DIR _site/site/
-jekyll build
--- _site/scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-zip -r website.zip _site
-
-curl -H "Content-Type: application/zip" \
-     -H "Authorization: Bearer $NETLIFYKEY" \
-     --data-binary "@website.zip" \
-     https://api.netlify.com/api/v1/sites/cv-emmanouil-brifas.netlify.app/deploys

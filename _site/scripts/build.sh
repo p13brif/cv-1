189fd1853230d2995584d05b9d2f55ba05ae7860
p13brif <p13brif@ionio.gr>
2021-04-04 18:52:54 +0300

    Update hooks

--- _site/scripts/build.sh
+++ _site/scripts/build.sh
@@ -4,8 +4,8 @@ set -e # halt script on error
 cd $TRAVIS_BUILD_DIR _site/site/
 jekyll build
 
-apt-get install build-essential python3-dev python3-pip python3-setuptools python3-wheel python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
+sudo apt-get install build-essential python3-dev python3-pip python3-setuptools python3-wheel python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 
-pip3 install weasyprint
+sudo pip3 install weasyprint
 
 weasyprint https://cv-emmanouil-brifas.netlify.app/ pdf/cv.pdf -s _template/pdf.css
